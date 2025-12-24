13075118b76e12915b9e366309a50c3c952b7a51
Filipe <114701203+FilipeSilvaCampos@users.noreply.github.com>
2025-12-24 04:55:21 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -125,7 +125,7 @@ function drawCartItems() {
       })
   
       const message = encodeURIComponent(`Olá gostaria de mais informações acerca desta cotação: ` + order);
-      const phone = "7399528587";
+      const phone = "73998313527";
   
       window.open(`https://wa.me/${phone}?text=${message}`)
   })
